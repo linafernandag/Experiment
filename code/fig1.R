5ee7eae2edc8f3b8836bad1c31059bc9d2aac31d
James Curley <jc3181@columbia.edu>
2018-11-19 15:25:12 -0600

    added figure 2

--- code/fig1.R
+++ code/fig1.R
@@ -6,3 +6,9 @@ library(tidyverse)
 head(mtcars)
 p<-ggplot(mtcars, aes(x=mpg, y=qsec,color=factor(cyl)))+geom_point()
 ggsave("img/fig1.png",p,width=10,height=10)
+
+
+#figure 2.
+
+p2<-ggplot(mtcars, aes(x=mpg, y=drat,color=factor(cyl)))+geom_point() + theme_minimal()
+ggsave("img/fig2.png",p2,width=10,height=10)
